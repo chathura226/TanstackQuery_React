a58f0955117dec1ccfd52c53e0a75cca5569fb82
chathura226 <chathuralakshan226@gmail.com>
2024-08-21 18:35:06 +0530

    useQueries and usequery fetching

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,12 @@
-import { useTodosIds } from '../services/queries'
+import {useTodos, useTodosIds} from '../services/queries'
+import {useIsFetching} from "@tanstack/react-query";
 
 function Todo() {
 
     const todosIdsQuery=useTodosIds();
+    const isFetching =useIsFetching()
+
+    const todosQueries=useTodos(todosIdsQuery.data);
 
     // if(todosIdsQuery.isPending){
     //     return <span>loading .....</span>
@@ -18,9 +22,28 @@ function Todo() {
         <p>Query Function status: {todosIdsQuery.fetchStatus}</p>
         {/*this will show the status of the query function*/}
         <p>Query data status: {todosIdsQuery.status}</p>
+        {/*this will show the number of fetchings globally*/}
+        <p>Global isFetching: {isFetching}</p>
         {todosIdsQuery.data?.map((id)=>(
-        <p key={id}>{id}</p>
-    ))}</div>
+        <p key={id}>id: {id}</p>
+    ))}
+
+        {todosQueries.map(({data})=>(
+            <li key={data?.id}>
+                <div>Id: {data?.id}</div>
+                <span>
+                    <strong>
+                        Title:
+                    </strong>
+                    {data?.title},{" "}
+                    <strong>
+                        Description:
+                    </strong>
+                    {data?.description},{" "}
+                </span>
+            </li>
+        ))}
+    </div>
   )
 }
 
--- src/services/queries.ts
+++ src/services/queries.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query"
-import { getTodosIds } from "./api"
+import {useQueries, useQuery} from "@tanstack/react-query"
+import {getTodo, getTodosIds} from "./api"
 
 export function useTodosIds(){
     return useQuery({
@@ -7,5 +7,16 @@ export function useTodosIds(){
         queryFn:getTodosIds,
         // refetchOnWindowFocus:false,//this will prevent the query from refetching when the window regains focus
 
+    })
+}
+
+export function useTodos(ids:(number | undefined)[]|undefined){
+    return useQueries({
+        queries:(ids??[]).map((id)=>{
+            return {
+                queryKey:['todo',id],
+                queryFn:()=>getTodo(id!),
+            }
+        })
     })
 }
